465f76edf260687709beb8b2dfe817d8d626f4aa
thedale3x11 <thedale.11.11.11@gmail.com>
2026-03-02 21:08:56 +0200

    gitignore added

--- no_money_exception.py
+++ no_money_exception.py
@@ -1,4 +1,8 @@
 class NoMoneyError(Exception):
-    def __init__(self) -> None:
-        self.message="No enough money."
+    message=None
+    def __init__(self):
+        if NoMoneyError.message==None:
+            self.message="No enough money."
+        else:
+            self.message=NoMoneyError.message
         super().__init__(self.message)
